f72830a83c61075fc5ef7dad205c50436376e5cd
Joeeyy <joeeeee@foxmail.com>
2018-08-21 14:06:12 +0800

    Initial Commit

---
+++ testVPN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testVPN
+//
+//  Created by Joe Liu on 2018/8/21.
+//  Copyright © 2018年 NUDT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
